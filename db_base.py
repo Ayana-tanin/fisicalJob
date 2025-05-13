000703702ffed955ba45f879d08d8faa0102b913
Ayana-tanin <ayana05j@gmail.com>
2025-05-13 13:28:20 +0600

    33

--- db_base.py
+++ db_base.py
@@ -4,16 +4,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
-# Отключаем проверку самоподписанных SSL-сертификатов Railway
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
-
 engine = create_async_engine(
     DATABASE_URL,
     echo=False,
     future=True,
-    connect_args={"ssl": ssl_context}  # Ключевой момент!
 )
 
 SessionLocal = sessionmaker(
